24d8ca4b68ababf3527b0015a2bafb8d8c4e0086
akshay dhangar <akshaygdhangar14@gmail.com>
2022-06-10 23:30:59 +0530

    UC2

---
+++ UC_2GetSize/BinarySearchTree.java
@@ -0,0 +1,60 @@
+package UC_2GetSize;
+
+public class BinarySearchTree<k extends Comparable<k>> {
+	MyBinaryNode<k> root;
+
+	
+	 // adding key to the tree
+	 
+	public void add(k key) {
+		this.root = this.insert(root, key);
+	}
+
+	
+	  //param root  is a root key for left and right side key
+	  //param value is value / data of tree node
+	  //return Adding key to the node of tree
+	 
+	private MyBinaryNode<k> insert(MyBinaryNode<k> root, k value) {
+		if (root == null) {
+			return new MyBinaryNode<>(value);
+		}
+		int compareResult = value.compareTo(root.value);
+		if (compareResult == 0) {
+			return root;
+		}
+		if (compareResult < 0) {
+			root.left = insert(root.left, value);
+		} else if (compareResult > 0) {
+			root.right = insert(root.right, value);
+		}
+		return root;
+
+	}
+	public int  getSize() {
+		return this.getSizeRecursive(root);
+	}
+	public int getSizeRecursive(MyBinaryNode<k> root) {
+		if(root==null) {
+			return 0;	
+		}
+		return 1 + getSizeRecursive(root.left)+getSizeRecursive(root.right);
+	}
+
+	
+	 // It displays the node
+	 
+
+	public void printNodes() {
+		printNodes(root);
+	}
+
+	private void printNodes(MyBinaryNode<k> root) {
+		if (root != null) {
+			printNodes(root.left);
+			System.out.println(root.value);
+			printNodes(root.right);
+
+		}
+	}
+}
---
+++ UC_2GetSize/MyBinaryNode.java
@@ -0,0 +1,19 @@
+package UC_2GetSize;
+
+public class MyBinaryNode<k extends Comparable<k>> {
+	k value;
+	MyBinaryNode<k> right;
+	MyBinaryNode<k> left;
+
+	/**
+	 * Constructor for node variable
+	 */
+
+	public MyBinaryNode(k value) {
+		this.value = value;
+		right = null;
+		left = null;
+	}
+
+}
+
